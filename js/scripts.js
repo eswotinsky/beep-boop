2f6b526cc1a4dca74b0f71668882bd9808740c5d
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-01-19 13:17:53 -0800

    update loop function to handle remaining nums containing 1 or 0; add div to display output

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ $(document).ready(function(){
   $("form#inputForm").submit(function(event){
     event.preventDefault();
 
-    var userInput = parseInt($("#numInput").val());
-    console.log(userInput);
+    //read & store user input
+    var userInput = ($("#numInput").val());
 
-    loopThroughInput(userInput);
+    $("#results").text(loopThroughInput(userInput));
   })
 
 });
@@ -14,14 +14,29 @@ $(document).ready(function(){
 function loopThroughInput(userInput) {
   var outputArray = [];
 
+  //loop through all nums between 0 and userInput
   for (var i = 0; i <= userInput; i++){
+
+    //replace nums divisible by 3 with "I'm sorry..." string
     if (i % 3 == 0 && i > 0) {
       outputArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
     }
+
+    //else, replace nums containing 1 with "Boop!" in outputArray
+    else if (i.toString().includes("1")) {
+      outputArray.push("Boop!");
+    }
+
+    //else, replace nums containing 0 with "Beep!" in outputArray
+    else if (i.toString().includes("0")) {
+      outputArray.push("Beep!");
+    }
+
+    //else, push the num in question to outputArray (as an int)
     else {
-      outputArray.push(i);
+      outputArray.push(parseInt(i));
     }
   }
-
   console.log(outputArray);
+  return outputArray;
 }
