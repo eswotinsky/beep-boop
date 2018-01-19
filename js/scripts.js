7924d2209fafd91b80aeeb564156fc62a2f0d65a
Eric Swotinsky <eric.swotinsky@gmail.com>
2018-01-19 10:29:12 -0800

    update loop function to replace non-0 nums divisible by 3 with a string

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
   $("form#inputForm").submit(function(event){
     event.preventDefault();
 
-    var userInput = $("#numInput").val();
+    var userInput = parseInt($("#numInput").val());
     console.log(userInput);
 
     loopThroughInput(userInput);
@@ -13,8 +13,15 @@ $(document).ready(function(){
 
 function loopThroughInput(userInput) {
   var outputArray = [];
-  for (var i = 0; i < userInput; i++){
-    outputArray.push(i);
+
+  for (var i = 0; i <= userInput; i++){
+    if (i % 3 == 0 && i > 0) {
+      outputArray.push("I'm sorry, Dave. I'm afraid I can't do that.");
+    }
+    else {
+      outputArray.push(i);
+    }
   }
+
   console.log(outputArray);
 }
